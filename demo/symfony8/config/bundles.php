df38d079232a01587a0aeac71395414ae7fd3247
GitHub Action <action@github.com>
2026-08-18 10:43:57 +0000

    Apply PHP CS Fixer fixes [skip ci]

--- demo/symfony8/config/bundles.php
+++ demo/symfony8/config/bundles.php
@@ -1,8 +1,8 @@
 <?php
 
 declare(strict_types=1);
-use Nowo\PdfSignableBundle\NowoPdfSignableBundle;
 use Nowo\HotReloadBundle\NowoHotReloadBundle;
+use Nowo\PdfSignableBundle\NowoPdfSignableBundle;
 use Nowo\TwigInspectorBundle\NowoTwigInspectorBundle;
 use Symfony\Bundle\DebugBundle\DebugBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
@@ -16,7 +16,7 @@ return [
     DebugBundle::class             => ['dev' => true, 'test' => true],
     WebProfilerBundle::class       => ['dev' => true, 'test' => true],
     NowoPdfSignableBundle::class   => ['all' => true],
-    NowoHotReloadBundle::class => ['dev' => true, 'test' => true],
+    NowoHotReloadBundle::class     => ['dev' => true, 'test' => true],
     NowoTwigInspectorBundle::class => ['dev' => true, 'test' => true],
     TwigExtraBundle::class         => ['all' => true],
 ];
